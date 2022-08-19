69118be4ec80e6669901a81b8cfcf60cf574b3d6
Mustafa Talha SAGLAM <talhasaglam153@gmail.com>
2022-08-19 10:22:36 +0300

    Updated Navigation

--- app/src/main/java/com/tcoding/socialapp/view/ui/fragment/SplashFragment.kt
+++ app/src/main/java/com/tcoding/socialapp/view/ui/fragment/SplashFragment.kt
@@ -1,15 +1,20 @@
 package com.tcoding.socialapp.view.ui.fragment
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.tcoding.socialapp.R
+import com.tcoding.socialapp.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
 
+    lateinit var binding: FragmentSplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,7 +27,16 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+       // binding.imageView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_logInMainFragment)
+
+        },5000)
+        return binding.root
+
 
     }
 
